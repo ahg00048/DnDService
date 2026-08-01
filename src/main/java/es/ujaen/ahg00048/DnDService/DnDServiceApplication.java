1bc8047dc97eb6b4f13037180802559f35ad904a
Antonio Huesa Guardiola <ahuesag@gmail.com>
2026-08-01 19:27:05 +0200

    primer commit

---
+++ src/main/java/es/ujaen/ahg00048/DnDService/DnDServiceApplication.java
@@ -0,0 +1,13 @@
+package es.ujaen.ahg00048.DnDService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ujaen/ahg00048/DnDService/DnDServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ujaen.ahg00048.DnDService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
