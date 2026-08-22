d8f7aca83ea4663d56f2f65566313c4404d36ce6
Antonio Huesa Guardiola <ahuesag@gmail.com>
2026-08-22 17:31:30 +0200

    eureka server y microservices clientes implementados

--- microservice-user/src/main/java/es/ujaen/ahg00048/microservice_user/app/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/es/ujaen/ahg00048/microservice_user/app/MicroserviceUserApplication.java
@@ -2,9 +2,9 @@ package es.ujaen.ahg00048.microservice_user.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
 @ComponentScan({"es.ujaen.ahg00048.microservice_user.service",
